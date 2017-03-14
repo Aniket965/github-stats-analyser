7e9ca0634c00f8d7dc2d026392872fe7c2a1d94a
aniket965 <aniket965.as@gmail.com>
2017-03-14 20:28:13 +0530

    completed bad ass

--- app.js
+++ app.js
@@ -7,20 +7,45 @@ var cheerio = require('cheerio');
 url = 'https://github.com/aniket965';
 //  requesting page
 
-request(url,function(error,res,html){
-if(!error){
+var days = [0, 0, 0, 0, 0, 0];
+request(url, function (error, res, html) {
+  if (!error) {
     //console.log(html);
-  var $ = cheerio.load(html);
+    var $ = cheerio.load(html);
     var dates = $('rect').nextAll();
-  fs.writeFileSync("aniket.txt",dates);
-var loader =   fs.readFileSync("aniket.txt");
-var u = cheerio.load(loader);
-var lol =  u('rect');
-while(lol != u('rect').last()){
-    console.log(lol.attr()['data-count']);
-    lol = lol.next();
-}
-console.log(lol);
-}
+    fs.writeFileSync("aniket.txt", dates);
+    var loader = fs.readFileSync("aniket.txt");
+    var u = cheerio.load(loader);
+    var lol = u('rect');
+    var counter = 0;
+    var c = 0;
+    while (lol.attr() != undefined) {
+      var value = parseInt(lol.attr()['data-count'] + '');
+      // console.log(value);    
+      days[counter] = days[counter] + value;
+      // console.log(days);
+      if (counter === 5) {
+        counter = 0;
+      }
+      else {
+        counter = counter + 1;
+      }
+
+      lol = lol.next();
+    }
+    var total_work = days.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+
+    var days_percentage = days.map(function (day) {
+      day = (day / total_work) * 100;
+      return day;
+    });
+    console.log(days_percentage);
+    // console.log(c);
+    console.log(total_work + ' exculding sundays');
+
+
+  }
 
 });
